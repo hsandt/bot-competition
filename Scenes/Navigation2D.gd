8012d1e40c5a85afae6b276a028010bd3d1faee8
hsandt <hs@fuwar.in>
2019-04-24 00:22:12 +0100

    [PATH] Moved navigation script and Line2D under Character, so that each character can have its own navigation path logic and render

--- Scenes/Navigation2D.gd
+++ Scenes/Navigation2D.gd
@@ -1,11 +1,5 @@
 extends Navigation2D
 
-export var radius = 2
-
-onready var line_2D : Line2D = $Line2D
-
-var path = []
-
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
@@ -16,15 +10,3 @@ func _ready():
 #	# Update game logic here.
 #	pass
 
-func _unhandled_input(event: InputEvent):
-	if event.is_action_pressed("set_move_target"):
-		for player in get_tree().get_nodes_in_group("player"):
-			print(player.global_position)
-			print(event.global_position)
-			path = get_simple_path(player.global_position, event.global_position)
-			line_2D.points = path
-			# path is relative, so in case this node is not at the origin, counter the offset
-			for i in range(path.size()):
-				line_2D.points[i] -= global_position
-			print(path)
-			update()
--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,9 +1,35 @@
 extends KinematicBody2D
 
+# Reference to common Navigation 2D for this level
+onready var navigation2D: Navigation2D = get_tree().get_nodes_in_group("navigation")[0]
+
+# Debug line showing the current path
+onready var line_2D : Line2D = $Line2D
+
+# Character speed
 export var speed = 2.0
 
+# Current navigation path
+var path = []
+
 func _ready():
-	pass
+	assert(line_2D != null)
 
 func _process(delta):
+	pass
+	
+func _unhandled_input(event: InputEvent):
+	if event.is_action_pressed("set_move_target"):
+		path = navigation2D.get_simple_path(global_position, event.global_position)
+		line_2D.points = path
+		print("path: " + str(path))
+		
+		# path is relative, so in case this node is not at the origin, counter the offset
+		for i in range(path.size()):
+			line_2D.points[i] -= navigation2D.global_position
+		
+		# make sure draw is called to update custom rendering besides the Line2D
+		update()
+
+func _draw():
 	pass
