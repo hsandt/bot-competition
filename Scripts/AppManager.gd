af4ae0d81cc7ba3f0583dd57ea4834e26cb7306a
huulong <n.huu.long@gmail.com>
2019-06-03 21:15:15 +0100

    [APP] Support hi-dpi with viewport scaling (for now, hard-coded 2x scale to work on 4k monitor)

--- Scripts/AppManager.gd
+++ Scripts/AppManager.gd
@@ -1,5 +1,15 @@
 extends Node
 
+func _ready():
+	print("viewport rect: " + str(get_viewport().get_visible_rect()))
+	print("Project settings dimensions: " + str(ProjectSettings.get_setting("display/window/size/width")))
+	print("window size: " + str(OS.get_window_size() ))
+	var displaySize = Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height"))
+	var scaleFactor = OS.get_window_size() / displaySize
+	print("scaleFactor: " + str(scaleFactor))
+	print("hi-dpi support: reset to " + str(displaySize / scaleFactor))
+	OS.set_window_size(displaySize * 2)
+
 func _input(event):
 	if event.is_action_pressed("app_exit"):
 		get_tree().quit()
